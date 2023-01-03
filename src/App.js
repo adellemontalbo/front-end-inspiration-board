d969c37d0e4c0a8a1ad1234a935bbb2738187009
dainiz <dainiz.almazan@gmail.com>
2023-01-03 13:06:48 -0800

    Creates files for all components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import CardList from "./components/CardList";
 import "./App.css";
 
+const EXAMPLE_CARD = {
+  cardId: 1,
+  boardId: 1,
+  message: "test message",
+  likesCount: 0,
+};
+
+const EXAMPLE_BOARD = {
+  boardId: 1,
+  title: "my first board",
+  owner: "jimbo",
+};
+
 function App() {
   return (
     <div className="App">
---
+++ src/components/Board.js
@@ -0,0 +1,9 @@
+import PropTypes from "prop-types";
+
+const Board = (props) => {
+  return <>WIP</>;
+};
+
+Board.propTypes = {};
+
+export default Board;
