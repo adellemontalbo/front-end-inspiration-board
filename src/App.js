40c24bfe036bae895498d4e0ce54009f1c20e76f
dainiz <dainiz.almazan@gmail.com>
2023-01-04 11:02:12 -0800

    Adds example data to App

--- src/App.js
+++ src/App.js
@@ -3,42 +3,58 @@ import CardList from "./components/CardList";
 import CardForm from "./components/CardForm";
 import BoardForm from "./components/BoardForm";
 import BoardList from "./components/BoardList";
+import Card from "./components/Card";
 
-const EXAMPLE_CARD = {
-  cardId: 1,
+const EXAMPLE_CARD_1 = {
+  id: 1,
   boardId: 1,
-  message: "test message",
+  message: "test message 1",
   likesCount: 0,
 };
 
-const EXAMPLE_BOARD = {
+const EXAMPLE_CARD_2 = {
+  id: 2,
   boardId: 1,
+  message: "test message 2",
+  likesCount: 0,
+};
+
+const EXAMPLE_BOARD_1 = {
+  id: 1,
   title: "my first board",
   owner: "jimbo",
-  cards: []
+  cards: [EXAMPLE_CARD_1, EXAMPLE_CARD_2],
 };
+
+const EXAMPLE_BOARD_2 = {
+  id: 2,
+  title: "my second board",
+  owner: "jimbo",
+  cards: [],
+};
+
+const EXAMPLE_BOARD_LIST = [EXAMPLE_BOARD_1, EXAMPLE_BOARD_2];
+
+const CARDS_ONLY = EXAMPLE_BOARD_1.cards;
 
 function App() {
   return (
     <div className="App">
-        <h1>Inspiration Board</h1>
-        <section classname="sidebar">
-          <BoardForm />
-          <BoardList />
-        </section>
-        <section>
-          <h2>BoardName and BoardOwner</h2>
-          <h2>Add New Card</h2>
-          <CardForm />
-          <div classname="cardDisplay">
-            CARDLIST
-          </div>
-        </section>
-        
-         
+      <h1>Inspiration Board</h1>
+      <section classname="sidebar">
+        {/* <BoardForm /> */}
+        <BoardList boardsData={EXAMPLE_BOARD_LIST} />
+      </section>
+      <section>
+        <h2>BoardName and BoardOwner</h2>
+        <h2>Add New Card</h2>
+        {/* <CardForm /> */}
+        <div classname="cardDisplay">
+          <CardList cardsData={CARDS_ONLY} />
+        </div>
+      </section>
     </div>
   );
 }
 
 export default App;
-
